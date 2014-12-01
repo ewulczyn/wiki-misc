117c8a9f3d9b355db586bae855dd8673f9b70556
ewulczyn <ellerywulczyn@gmail.com>
2014-11-30 18:25:02 -0800

    Second draft

--- self_terminating_ab/src/python/abstract_abtest.py
+++ self_terminating_ab/src/python/abstract_abtest.py
@@ -139,9 +139,9 @@ def expected_results_by_lift(TestClass, params, iters, p_hat, lifts, fig_name=No
     df = pd.DataFrame.from_records(data, columns=columns)
 
     ax1.set_ylim([-0.1, 1.1])
-    ax1.plot(lifts, p_A_betters['mean'], label='P(A better) median')
-    ax1.plot(lifts, p_A_betters['lower'], label='P(A better) lower', alpha=0.31)
-    ax1.plot(lifts, p_A_betters['upper'], label='P(A better) upper', alpha=0.31)
+    ax1.plot(lifts, p_A_betters['mean'], label='P(A wins) median')
+    ax1.plot(lifts, p_A_betters['lower'], label='P(A wins) lower', alpha=0.31)
+    ax1.plot(lifts, p_A_betters['upper'], label='P(A wins) upper', alpha=0.31)
 
     ax1.plot(lifts, p_unknowns['mean'], label='P(unknown)')
     ax1.set_xlabel('percent lift')
@@ -159,3 +159,89 @@ def expected_results_by_lift(TestClass, params, iters, p_hat, lifts, fig_name=No
         fig.savefig(fig_name)
 
     return df
+
+
+
+
+
+def expected_results_by_interval(TestClass, params, iters, p_hat, lifts, fig_name=None):
+
+    """
+    This function generates plots that show the expected results
+    of the AB test as you change the lift that banner A has over
+    banner B.
+    """
+
+    # see how you would do in practice
+    run_times_list = {"lower": [], "upper":[], "mean": []}
+    p_A_betters = {"lower": [], "upper":[], "mean": []}
+
+    (lower, mean, upper) = p_hat.p_donate_ci(10)
+
+    for lift in lifts:
+        print lift
+
+        p_B = p_hat
+
+        # mean
+        new_params = list(params)
+        new_params[0] = SimStream(p_B.lift(lift)) #a_stream
+        new_params[1] = SimStream(p_B) #b_stream
+        p_better, p_unknown, time = expected_results(TestClass, new_params, iters)
+        run_times_list['mean'].append(time)
+        p_A_betters['mean'].append(p_better)
+
+
+        #lower
+        new_params = list(params)
+        new_params[0] = SimStream(p_B.lift(lift)) #a_stream
+        new_params[1] = SimStream(p_B) #b_stream
+        new_params[2] = 10
+        p_better, p_unknown, time = expected_results(TestClass, new_params, iters)
+        p_A_betters['lower'].append(p_better)
+        run_times_list['lower'].append(time)
+        
+
+        #upper
+        new_params = list(params)
+        new_params[0] = SimStream(p_B.lift(lift)) #a_stream
+        new_params[1] = SimStream(p_B) #b_stream
+        new_params[2] = params[3] + 1
+        p_better, p_unknown, time = expected_results(TestClass, new_params, iters)
+        p_A_betters['upper'].append(p_better)
+        run_times_list['upper'].append(time)
+
+    lifts = np.array(lifts)*100
+
+    avg_run_times_mean = np.array([np.mean(run_times) for run_times in run_times_list['mean']])
+    avg_run_times_upper = np.array([np.mean(run_times) for run_times in run_times_list['upper']])
+    avg_run_times_lower = np.array([np.mean(run_times) for run_times in run_times_list['lower']])
+
+
+    fig = plt.figure(figsize=(13, 8))
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+
+
+
+    ax1.set_ylim([-0.1, 1.1])
+    ax1.plot(lifts, p_A_betters['mean'], label='P(A wins) n = %d' % params[2])
+    ax1.plot(lifts, p_A_betters['lower'], label='P(A wins) n = 10', alpha=0.31)
+    ax1.plot(lifts, p_A_betters['upper'], label='P(A wins) n = %d' % params[3], alpha=0.31)
+
+    ax1.set_xlabel('percent lift')
+    ax1.set_ylabel('probability of choosing A')
+    ax1.legend(loc=4)
+
+    ax2.set_xlim([lifts[0], lifts[-1]])
+    ax2.plot(lifts, avg_run_times_mean, label='n = %d'% params[2])
+    ax2.plot(lifts, avg_run_times_upper, label='n = %d' % params[3])
+    ax2.plot(lifts, avg_run_times_lower, label='n = 1')
+
+    ax2.set_xlabel('percent lift')
+    ax2.set_ylabel('impressions per banner')
+    ax2.legend()
+    plt.show()
+    if fig_name:
+        fig.savefig(fig_name)
+
