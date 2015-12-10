c57503379647f95ab9fa44f89332b3079d55c35d
Ellery Wulczyn <ellerywulczyn@Ellery-Wulczyns-MacBook-Pro.local>
2015-12-10 12:41:15 -0800

    adding latest fr code

--- record_impression/oozie/commads.sh
+++ record_impression/oozie/commads.sh
@@ -18,3 +18,11 @@ hadoop fs -mkdir /user/ellery/record_impression/oozie
 hadoop fs -rm -r -f  /user/ellery/record_impression/oozie/*
 hadoop fs -put ~/wmf/record_impression/oozie/* /user/ellery/record_impression/oozie/
 oozie job -oozie http://analytics1027.eqiad.wmnet:11000/oozie -run -config ~/wmf/record_impression/oozie/coordinator.properties
+
+
+
+set hivevar:version=test;
+set hivevar:year=2015;
+set hivevar:month=12;
+set hivevar:day=1;
+set hivevar:hour=1;
--- record_impression/oozie/hive_query.sql
+++ record_impression/oozie/hive_query.sql
@@ -4,7 +4,10 @@ USE ${database};
 
 CREATE TABLE IF NOT EXISTS ellery.oozie_impressions_${version} (
   banner STRING,
-  minute STRING,
+  campaign STRING,
+  referer STRING,
+  country STRING,
+  dt STRING,
   n INT
 )
 PARTITIONED BY (year INT, month INT, day INT);
@@ -15,36 +18,44 @@ DROP TABLE IF EXISTS ellery.oozie_impressions_${year}_${month}_${day}_${hour}_${
 --This Is Really a Temporary Table need 0.14
 CREATE TABLE  ellery.oozie_impressions_${year}_${month}_${day}_${hour}_${version} (
   banner STRING,
-  minute STRING,
+  campaign STRING,
+  referer STRING,
+  country STRING,
+  dt STRING,
   n INT
 );
 
 INSERT INTO TABLE ellery.oozie_impressions_${year}_${month}_${day}_${hour}_${version}
-
 SELECT 
   a.banner,
-  a.minute, 
+  a.campaign,
+  a.referer,
+  a.country,
+  a.dt,
   COUNT(*) as n 
 FROM (
     SELECT
       PARSE_URL(CONCAT('http://bla.org/woo/', uri_query), 'QUERY', 'banner') as banner,
-      SUBSTR(dt, 0, 16) as minute
+      PARSE_URL(CONCAT('http://bla.org/woo/', uri_query), 'QUERY', 'campaign') as campaign,
+      referer,
+      PARSE_URL(CONCAT('http://bla.org/woo/', uri_query), 'QUERY', 'country') as country,
+      concat(year,'-',month,'-',day, ' ', hour, ':00') as dt
     FROM wmf.webrequest 
     WHERE year = ${year}
       AND month = ${month}
       AND day = ${day}
       AND hour = ${hour}
       AND ((webrequest_source = 'text') OR (webrequest_source = 'mobile'))
-      AND uri_host in ('meta.wikimedia.org', 'meta.m.wikimedia.org')
-      AND uri_path = '/w/index.php'
-      AND PARSE_URL(CONCAT('http://bla.org/woo/', uri_query), 'QUERY', 'title') IS NOT NULL
-      AND PARSE_URL(CONCAT('http://bla.org/woo/', uri_query), 'QUERY', 'title') = 'Special:BannerLoader'
+      AND uri_path = '/beacon/impression'
+      AND PARSE_URL(CONCAT('http://bla.org/woo/', uri_query), 'QUERY', 'result') = 'show'
       AND agent_type = 'user'
-
 ) a
 GROUP BY
   a.banner,
-  a.minute;
+  a.campaign,
+  a.referer,
+  a.country,
+  a.dt;
 
  
 INSERT INTO TABLE ellery.oozie_impressions_${version}
